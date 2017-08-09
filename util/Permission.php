9cf634dc067cb2b5850c62922b4860963a5b22d2
Felideo <felideo@gmail.com>
2017-08-09 00:08:41 -0300

    FIX - LeafLiveDB * ajuste!

--- util/Permission.php
+++ util/Permission.php
@@ -8,8 +8,8 @@ class Permission {
 			if(empty($_SESSION['permissoes'][$modulo]) || empty($_SESSION['permissoes'][$modulo][$permissao])){
 				$view = new Libs\View();
 				$view->alert_js('Vocã não possui permissão para efetuar esta ação...', 'erro');
-				header('location: ' . URL . 'painel_controle');
-				exit;
+				// header('location: ../index');
+				// exit;
 			}
 		}
 	}
