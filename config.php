10cc907ffe2dfd8d58cfe23d2f2e7cdeccd37477
Felideo <felideo@gmail.com>
2017-08-09 00:19:42 -0300

    FIX - LeafLiveDB * ajuste!

--- config.php
+++ config.php
@@ -10,17 +10,17 @@ define('URL', 'http://leaflivedb.localhost');
 define('LIBS', 'libs/');
 
 // Configuração com Banco de Dados
-define('DB_TYPE', 'mysql');
-define('DB_HOST', '127.0.0.1');
-define('DB_NAME', 'LeafLiveDB');
-define('DB_USER', 'root');
-define('DB_PASS', 'lilith');
-
 // define('DB_TYPE', 'mysql');
-// define('DB_HOST', 'mysql.hostinger.com.br');
-// define('DB_NAME', 'u595159613_leaf');
-// define('DB_USER', 'u595159613_leaf');
-// define('DB_PASS', '20lilith88');
+// define('DB_HOST', '127.0.0.1');
+// define('DB_NAME', 'LeafLiveDB');
+// define('DB_USER', 'root');
+// define('DB_PASS', 'lilith');
+
+define('DB_TYPE', 'mysql');
+define('DB_HOST', 'mysql.hostinger.com.br');
+define('DB_NAME', 'u595159613_leaf');
+define('DB_USER', 'u595159613_leaf');
+define('DB_PASS', '20lilith88');
 
 define('APP_NAME', 'LeafLiveDB');
 
